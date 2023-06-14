ffd73b750b0afe8c04375fe4bb3fcd0be14d9f43
JyothisKiran <savannah.j@codelynks.com>
2023-06-14 18:00:19 +0530

    bd constraints

--- src/hall/urls.py
+++ src/hall/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import success_page,hall_booked_status_view,booking_cancel_view,booking_update_view,meeting_create_view
 
 urlpatterns = [
-    path('hallbooking/',meeting_create_view, name='hallbooking'),
+    path('booking/',meeting_create_view, name='hallbooking'),
     path('sucess/',success_page,name ='success'),
     path('bookinglist/',hall_booked_status_view,name ='bookinglist'),
     path('bookingupdate/<int:pk>/',booking_update_view, name='bookingupdate'),
--- src/hall/views.py
+++ src/hall/views.py
@@ -7,28 +7,6 @@ from django.shortcuts import get_object_or_404,HttpResponse
 # Create your views here.
 
 
-# def meeting_create_view(request):
-#     if request.method == 'POST':
-#         form = HallBookingForm(request.POST)
-#         if form.is_valid():
-#             start_time = form.cleaned_data['start_time']
-#             end_time = form.cleaned_data['end_time']
-#             room = form.cleaned_data['room']
-#             team = form.cleaned_data['team_name']
-#             date = form.cleaned_data['date']
-
-#             if is_room_available(start_time, end_time, room,date) and (start_time <end_time):
-#                 Meeting.objects.create(start_time=start_time, end_time=end_time, room=room,team_name=team,date = date)
-#                 form = HallBookingForm()
-#                 return render (request,'sucess.html',{'start_time':start_time,'end_time':end_time,'room':room, 'date':date ,'team':team })
-#             else:
-#                 error_message = 'The room is not available for the selected time frame.'
-#                 return render(request, 'hallbook.html', {'form': form, 'error_message': error_message})
-#     else:
-#         form = HallBookingForm()
-    
-#     return render(request, 'hallbook.html', {'form': form})
-
 def meeting_create_view(request):
     if request.method == 'POST':
         form = HallBookingForm(request.POST)
